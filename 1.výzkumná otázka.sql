f40d5a4d88be835e719537c81c73e388e708a86e
MisaVaskova <155457837+MisaVaskova@users.noreply.github.com>
2024-11-13 19:31:18 +0100

    Update 1.výzkumná otázka.sql
    Doplnění scriptu pro první otázku, tak aby bylo jasné porovnání dat.

--- 1.výzkumná otázka.sql
+++ 1.výzkumná otázka.sql
@@ -1,8 +1,12 @@
-SELECT DISTINCT 
+SELECT 
     industry_branch_name,
     payroll_year,
-    average_salary_per_year 
-FROM
-    t_Michaela_Vaskova_project_SQL_primary_final tzvpspf 
-ORDER BY
+    AVG(average_salary_per_year) AS average_salary_per_year,
+    COALESCE(LAG(AVG(average_salary_per_year), 1) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year), 0) AS previous_payroll_year,
+    AVG(average_salary_per_year) - COALESCE(LAG(AVG(average_salary_per_year), 1) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year), 0) AS payroll_change
+FROM 
+    t_Michaela_Vaskova_project_SQL_primary_final
+GROUP BY 
+    industry_branch_name, payroll_year
+ORDER BY 
     industry_branch_name, payroll_year;
